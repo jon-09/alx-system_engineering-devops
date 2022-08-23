b84d2c6e005a5c25fdabcf680559c39ef22de2b6
Jonathan <achola.jon@yahoo.com>
2022-08-23 16:41:40 +0300

    simply matching school

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,6 +1,2 @@
-
-
 #!/usr/bin/env ruby
-# Script that accepts one argument and pass it to a regular expression matching method
-# The regular expression must match Holberton
 puts ARGV[0].scan(/School/).join
